b20b3bf76279f15a4d93ca81bdf8341f3a2c263e
RaphaelLeivas <raphaelhenrique2013@outlook.com>
2025-11-04 12:47:50 -0300

    avanços experimentos lab2_p2

--- scratch/lab2_p2.cc
+++ scratch/lab2_p2.cc
@@ -13,166 +13,194 @@ using namespace ns3;
 int
 main(int argc, char* argv[])
 {
-    RngSeedManager::SetSeed(time(NULL));
+    int number_of_experiments = 10; 
+    std::ofstream file("experiment_lab2p2.csv");
 
-    LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
-    LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
-
-    std::string dataRate = "1Mbs";
-    std::string delay = "20ms";
-    double errorRate = 0.00001;
-    uint32_t nFlows = 6;
-    std::string transport_prot = "ns3::TcpCubic"; // ou ns3::TcpNewReno
-    double simTime = 10.0;
-
-    CommandLine cmd;
-    cmd.AddValue("dataRate", "Taxa de envio de dados", dataRate);
-    cmd.AddValue("delay", "Atraso no envio de dados", delay);
-    cmd.AddValue("errorRate", "Taxa de erros", errorRate);
-    cmd.AddValue("nFlows", "Numero de fluxos", nFlows);
-    cmd.AddValue("transport_prot", "Protocolo", transport_prot);
-    cmd.Parse(argc, argv);
-
-    if (nFlows > MAX_FLOWS)
-    {
-        nFlows = MAX_FLOWS;
+    if (!file.is_open()) {
+        std::cerr << "Error opening file!" << std::endl;
+        return 1;
     }
 
-    Config::SetDefault("ns3::TcpL4Protocol::SocketType", StringValue(transport_prot));
+    // Write header
+    file << "exp,media_sa,media_sb\n";
 
-    NodeContainer nodes;
-    nodes.Create(N_NODES); // 0: dest, 1: bottleneck1 b1,  2: bottleneck1 b2, 3: Servidor A, 4: Servidor B
+    for (int k = 0; k < number_of_experiments; ++k) {
+        RngSeedManager::SetSeed(time(NULL));
 
-    /*  --------- Links entre os nos -----------   */
-    // entre dest e b1
-    PointToPointHelper p2p_db1;
-    p2p_db1.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
-    p2p_db1.SetChannelAttribute("Delay", StringValue("0.01ms"));
+        LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+        LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
-    // entre b1 e b2 - bottleneck
-    PointToPointHelper p2p_b1b2;
-    p2p_b1b2.SetDeviceAttribute("DataRate", StringValue(dataRate));
-    p2p_b1b2.SetChannelAttribute("Delay", StringValue(delay));
-    Ptr<RateErrorModel> em = CreateObject<RateErrorModel> ();
-    em->SetAttribute ("ErrorRate", DoubleValue (errorRate));
-    p2p_b1b2.SetDeviceAttribute("ReceiveErrorModel", PointerValue(em));
+        std::string dataRate = "1Mbs";
+        std::string delay = "20ms";
+        double errorRate = 0.00001;
+        uint32_t nFlows = 6;
+        std::string transport_prot = "ns3::TcpCubic"; // ou ns3::TcpNewReno
+        double simTime = 10.0;
 
-    // entre b2 e SA
-    PointToPointHelper p2p_b2sa;
-    p2p_b2sa.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
-    p2p_b2sa.SetChannelAttribute("Delay", StringValue("0.01ms"));
+        CommandLine cmd;
+        cmd.AddValue("dataRate", "Taxa de envio de dados", dataRate);
+        cmd.AddValue("delay", "Atraso no envio de dados", delay);
+        cmd.AddValue("errorRate", "Taxa de erros", errorRate);
+        cmd.AddValue("nFlows", "Numero de fluxos", nFlows);
+        cmd.AddValue("transport_prot", "Protocolo", transport_prot);
+        cmd.Parse(argc, argv);
 
-    // entre b2 e SB
-    PointToPointHelper p2p_b2sb;
-    p2p_b2sb.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
-    p2p_b2sb.SetChannelAttribute("Delay", StringValue("50ms"));
+        if (nFlows > MAX_FLOWS)
+        {
+            nFlows = MAX_FLOWS;
+        }
 
-    /*  --------- containers dos nos -----------   */
+        Config::SetDefault("ns3::TcpL4Protocol::SocketType", StringValue(transport_prot));
 
-    NodeContainer node_db1 = NodeContainer(nodes.Get(0), nodes.Get(1));
-    NodeContainer node_b1b2 = NodeContainer(nodes.Get(1), nodes.Get(2));
-    NodeContainer node_b2sa = NodeContainer(nodes.Get(2), nodes.Get(3));
-    NodeContainer node_b2sb = NodeContainer(nodes.Get(2), nodes.Get(4));
+        NodeContainer nodes;
+        nodes.Create(N_NODES); // 0: dest, 1: bottleneck1 b1,  2: bottleneck1 b2, 3: Servidor A, 4: Servidor B
 
-    NetDeviceContainer net_db1 = p2p_db1.Install(node_db1);
-    NetDeviceContainer net_b1b2 = p2p_b1b2.Install(node_b1b2);
-    NetDeviceContainer net_b2sa = p2p_b2sa.Install(node_b2sa);
-    NetDeviceContainer net_b2sb = p2p_b2sb.Install(node_b2sb);
+        /*  --------- Links entre os nos -----------   */
+        // entre dest e b1
+        PointToPointHelper p2p_db1;
+        p2p_db1.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
+        p2p_db1.SetChannelAttribute("Delay", StringValue("0.01ms"));
 
-    /*  --------- endereçamento IP ----------   */
+        // entre b1 e b2 - bottleneck
+        PointToPointHelper p2p_b1b2;
+        p2p_b1b2.SetDeviceAttribute("DataRate", StringValue(dataRate));
+        p2p_b1b2.SetChannelAttribute("Delay", StringValue(delay));
+        Ptr<RateErrorModel> em = CreateObject<RateErrorModel> ();
+        em->SetAttribute ("ErrorRate", DoubleValue (errorRate));
+        p2p_b1b2.SetDeviceAttribute("ReceiveErrorModel", PointerValue(em));
 
-    InternetStackHelper stack;
-    stack.Install(nodes);
+        // entre b2 e SA
+        PointToPointHelper p2p_b2sa;
+        p2p_b2sa.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
+        p2p_b2sa.SetChannelAttribute("Delay", StringValue("0.01ms"));
 
-    Ipv4AddressHelper address;
+        // entre b2 e SB
+        PointToPointHelper p2p_b2sb;
+        p2p_b2sb.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
+        p2p_b2sb.SetChannelAttribute("Delay", StringValue("50ms"));
 
-    address.SetBase("10.1.1.0", "255.255.255.0");
-    Ipv4InterfaceContainer interface_db1 = address.Assign(net_db1);
+        /*  --------- containers dos nos -----------   */
 
-    address.SetBase("10.1.2.0", "255.255.255.0");
-    Ipv4InterfaceContainer interface_b1b2 = address.Assign(net_b1b2);
+        NodeContainer node_db1 = NodeContainer(nodes.Get(0), nodes.Get(1));
+        NodeContainer node_b1b2 = NodeContainer(nodes.Get(1), nodes.Get(2));
+        NodeContainer node_b2sa = NodeContainer(nodes.Get(2), nodes.Get(3));
+        NodeContainer node_b2sb = NodeContainer(nodes.Get(2), nodes.Get(4));
 
-    address.SetBase("10.1.3.0", "255.255.255.0");
-    Ipv4InterfaceContainer interface_b2sa = address.Assign(net_b2sa);
+        NetDeviceContainer net_db1 = p2p_db1.Install(node_db1);
+        NetDeviceContainer net_b1b2 = p2p_b1b2.Install(node_b1b2);
+        NetDeviceContainer net_b2sa = p2p_b2sa.Install(node_b2sa);
+        NetDeviceContainer net_b2sb = p2p_b2sb.Install(node_b2sb);
 
-    address.SetBase("10.1.4.0", "255.255.255.0");
-    Ipv4InterfaceContainer interface_b2sb = address.Assign(net_b2sb);
+        /*  --------- endereçamento IP ----------   */
 
-    // sink: destino de um fluxo de dados
-    // Vector to store the sink applications for later goodput calculation
-    std::vector<Ptr<PacketSink>> sinks;
+        InternetStackHelper stack;
+        stack.Install(nodes);
 
-    uint16_t port = 50000;
-    uint32_t halfFlows = nFlows / 2;
+        Ipv4AddressHelper address;
 
-    // configura a aplicação sink no servidor A
-    for (uint32_t i = 0; i < halfFlows; i++)
-    {
-        Address sinkAddr(InetSocketAddress(interface_b2sa.GetAddress(1), port + i));
-        PacketSinkHelper sinkHelper("ns3::TcpSocketFactory", sinkAddr);
-        ApplicationContainer sinkApp = sinkHelper.Install(nodes.Get(3));
-        sinkApp.Start(Seconds(1.0));
-        sinkApp.Stop(Seconds(simTime));
+        address.SetBase("10.1.1.0", "255.255.255.0");
+        Ipv4InterfaceContainer interface_db1 = address.Assign(net_db1);
 
-        sinks.push_back(DynamicCast<PacketSink>(sinkApp.Get(0)));
+        address.SetBase("10.1.2.0", "255.255.255.0");
+        Ipv4InterfaceContainer interface_b1b2 = address.Assign(net_b1b2);
+
+        address.SetBase("10.1.3.0", "255.255.255.0");
+        Ipv4InterfaceContainer interface_b2sa = address.Assign(net_b2sa);
+
+        address.SetBase("10.1.4.0", "255.255.255.0");
+        Ipv4InterfaceContainer interface_b2sb = address.Assign(net_b2sb);
+
+        // sink: destino de um fluxo de dados
+        // Vector to store the sink applications for later goodput calculation
+        std::vector<Ptr<PacketSink>> sinks;
+
+        uint16_t port = 50000;
+        uint32_t halfFlows = nFlows / 2;
+
+        // configura a aplicação sink no servidor A
+        for (uint32_t i = 0; i < halfFlows; i++)
+        {
+            Address sinkAddr(InetSocketAddress(interface_b2sa.GetAddress(1), port + i));
+            PacketSinkHelper sinkHelper("ns3::TcpSocketFactory", sinkAddr);
+            ApplicationContainer sinkApp = sinkHelper.Install(nodes.Get(3));
+            sinkApp.Start(Seconds(1.0));
+            sinkApp.Stop(Seconds(simTime));
+
+            sinks.push_back(DynamicCast<PacketSink>(sinkApp.Get(0)));
+        }
+
+        // configura a aplicação sink no servidor B
+        for (uint32_t i = halfFlows; i < nFlows; i++)
+        {
+            Address sinkAddr(InetSocketAddress(interface_b2sb.GetAddress(1), port + i));
+            PacketSinkHelper sinkHelper("ns3::TcpSocketFactory", sinkAddr);
+            ApplicationContainer sinkApp = sinkHelper.Install(nodes.Get(4));
+            sinkApp.Start(Seconds(1.0));
+            sinkApp.Stop(Seconds(simTime));
+
+            sinks.push_back(DynamicCast<PacketSink>(sinkApp.Get(0)));
+        }
+
+        // configura a aplicação source
+        for (uint32_t i = 0; i < nFlows; i++)
+        {
+            // uma hora vai pro A, outra vai pro B
+            Ipv4Address destIp = (i < halfFlows) ? interface_b2sa.GetAddress(1) : interface_b2sb.GetAddress(1);
+            uint16_t destPort = port + i;
+
+            BulkSendHelper source("ns3::TcpSocketFactory",
+                            InetSocketAddress(destIp, destPort));
+
+            source.SetAttribute("MaxBytes", UintegerValue(0));
+            ApplicationContainer srcApp = source.Install(nodes.Get(0));
+            srcApp.Start(Seconds(1.0 + 0.1 * i));
+            srcApp.Stop(Seconds(simTime));
+        }
+
+        Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+
+        // define o XML para onde vai a animação
+        // AnimationInterface anim("lab2_p2.xml");
+
+        // anim.SetConstantPosition(nodes.Get(0), 10.0, 30.0);
+        // anim.SetConstantPosition(nodes.Get(1), 30.0, 30.0);
+        // anim.SetConstantPosition(nodes.Get(2), 50.0, 30.0);
+        // anim.SetConstantPosition(nodes.Get(3), 70.0, 10.0);
+        // anim.SetConstantPosition(nodes.Get(4), 70.0, 50.0);
+
+        Simulator::Stop(Seconds(simTime));
+        Simulator::Run();
+
+        std::cout << "----- Calcula o Goodput -----" << std::endl;
+
+        double gpA_media = 0;
+        double gpB_media = 0;
+
+        for (uint32_t i = 0; i < nFlows; i++)
+        {
+            double bytesReceived = sinks[i]->GetTotalRx();
+            double goodputMbps = (bytesReceived * 8) / (simTime * 1e6);
+
+            if (i < halfFlows) {
+                gpA_media += goodputMbps / halfFlows;
+            } else {
+                gpB_media += goodputMbps / halfFlows;
+            }
+
+            // std::cout << "FLuxo " << i
+            //         << " recebeu: " << bytesReceived << " bytes, "
+            //         << "Goodput = " << goodputMbps << " Mbps"
+            //         << (i < halfFlows ? " (Servidor A (3))" : " (Servidor B (4))") 
+            //         << std::endl;
+        }
+
+        file << k << "," << gpA_media << "," << gpB_media << "\n";
+
+        Simulator::Destroy();
     }
 
-    // configura a aplicação sink no servidor B
-    for (uint32_t i = halfFlows; i < nFlows; i++)
-    {
-        Address sinkAddr(InetSocketAddress(interface_b2sb.GetAddress(1), port + i));
-        PacketSinkHelper sinkHelper("ns3::TcpSocketFactory", sinkAddr);
-        ApplicationContainer sinkApp = sinkHelper.Install(nodes.Get(4));
-        sinkApp.Start(Seconds(1.0));
-        sinkApp.Stop(Seconds(simTime));
+    file.close();
+    std::cout << "CSV file created successfully!" << std::endl;
 
-        sinks.push_back(DynamicCast<PacketSink>(sinkApp.Get(0)));
-    }
-
-    // configura a aplicação source
-    for (uint32_t i = 0; i < nFlows; i++)
-    {
-        // uma hora vai pro A, outra vai pro B
-        Ipv4Address destIp = (i < halfFlows) ? interface_b2sa.GetAddress(1) : interface_b2sb.GetAddress(1);
-        uint16_t destPort = port + i;
-
-        BulkSendHelper source("ns3::TcpSocketFactory",
-                        InetSocketAddress(destIp, destPort));
-
-        source.SetAttribute("MaxBytes", UintegerValue(0));
-        ApplicationContainer srcApp = source.Install(nodes.Get(0));
-        srcApp.Start(Seconds(1.0 + 0.1 * i));
-        srcApp.Stop(Seconds(simTime));
-    }
-
-    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
-
-    // define o XML para onde vai a animação
-    AnimationInterface anim("lab2_p2.xml");
-
-    anim.SetConstantPosition(nodes.Get(0), 10.0, 30.0);
-    anim.SetConstantPosition(nodes.Get(1), 30.0, 30.0);
-    anim.SetConstantPosition(nodes.Get(2), 50.0, 30.0);
-    anim.SetConstantPosition(nodes.Get(3), 70.0, 10.0);
-    anim.SetConstantPosition(nodes.Get(4), 70.0, 50.0);
-
-    Simulator::Stop(Seconds(simTime));
-    Simulator::Run();
-
-    std::cout << "----- Goodput Results -----" << std::endl;
-
-    for (uint32_t i = 0; i < nFlows; i++)
-    {
-        double bytesReceived = sinks[i]->GetTotalRx();
-        double goodputMbps = (bytesReceived * 8) / (simTime * 1e6);
-
-        std::cout << "Flow " << i
-                << " received: " << bytesReceived << " bytes, "
-                << "Goodput = " << goodputMbps << " Mbps"
-                << (i < halfFlows ? " (Dest1)" : " (Dest2)") 
-                << std::endl;
-    }
-
-    Simulator::Destroy();
     return 0;
 }
